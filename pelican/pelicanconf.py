7552cadf423386d82953bc0abc2429763e5c1d11
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-11-04 19:18:41 -0700

    add pelicanconf.py

---
+++ pelican/pelicanconf.py
@@ -0,0 +1,68 @@
+import os
+
+
+INFINITE_PELICAN_VERSION = "0.1.0"
+
+
+# Yes, this is supposed to be UI not URI...
+SITEURL = os.environ.get('INFINITE_BASE_UI', '')
+
+AUTHOR = u'Ch4zm of Hellmouth'
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+SITENAME = u'Infinite Cleveland'
+
+PATH = 'content'
+THEME = 'golly-pelican-theme'
+
+# Don't try to turn HTML files into pages
+READERS = {'html': None}
+
+# Static stuff
+STATIC_PATHS = ['img']
+
+
+# --------------------
+# Map template pages to their final file name
+
+TEMPLATE_PAGES = {}
+
+TEMPLATE_PAGES['index.html'] = 'index.html'
+TEMPLATE_PAGES['index.js'] = 'index.js'
+
+# --------------------
+# Add custom routes
+
+THEME_TEMPLATES_OVERRIDES = []
+#THEME_TEMPLATES_OVERRIDES.append('simulator')
+
+#TEMPLATE_PAGES['simulator/rainbow.html'] = 'simulator/index.html'
+#TEMPLATE_PAGES['simulator/rainbow.js']   = 'simulator/rainbow.js'
+
+# --------------------
+# SHUT UP
+
+ARCHIVES_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+DAY_SAVE_AS = ''
+INDEX_SAVE_AS = ''
+MONTH_ARCHIVE_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+YEAR_ARCHIVE_SAVE_AS = ''
+
+
+# ---------------------
+
+# No feeds
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+DEFAULT_PAGINATION = False
+TIMEZONE = 'America/Los_Angeles'
+
